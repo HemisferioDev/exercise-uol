a8b1ca09f6e9b2e64ef743444ec5710103dc7aeb
Josué Daniel Flores <mr.danielfo@gmail.com>
2021-02-01 00:23:17 -0600

    js file ready

---
+++ index.js
@@ -0,0 +1,4 @@
+const listIcon = document.querySelector('.bi.bi-list-ul')
+const exerciseContainer = document.querySelector('.exercise-container');
+const messageContainer = document.querySelector('.message-container');
+const sendButton = document.querySelector('.btnSend');
